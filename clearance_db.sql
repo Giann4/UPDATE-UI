e67820dd62f7502ac03b24a5eee51461a1c37bff
Giann4 <gianbenedictesio@gmail.com>
2026-04-24 12:31:51 +0800

    Add files via upload

--- clearance_db.sql
+++ clearance_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2026 at 10:15 AM
+-- Generation Time: Apr 24, 2026 at 06:30 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,7 +41,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `name`, `email`, `password`, `role`, `profile_photo`) VALUES
-(1, 'Admin', 'admin@gmail.com', '0192023a7bbd73250516f069df18b500', 'admin', 'admin_1_1776331551.png');
+(1, 'Admin', 'admin@gmail.com', 'c93ccd78b2076528346216b3b2f701e6', 'admin', 'admin_1_1776331551.png');
 
 -- --------------------------------------------------------
 
@@ -88,7 +88,10 @@ INSERT INTO `class_requests` (`id`, `class_id`, `student_id`, `subject`, `status
 (8, 8, 3, 'CALCULUS', 'Reviewed', 'Passed', 'CONGRATS', '2026-04-12', '2026-04-12 15:58:55'),
 (9, 9, 3, 'PE', 'Reviewed', 'Passed', '1234', '2026-04-12', '2026-04-12 16:06:33'),
 (10, 10, 12, 'SCIENCE', 'Reviewed', 'Passed', 'CONGRATS', '2026-04-12', '2026-04-12 17:53:36'),
-(11, 11, 14, 'AP', 'Reviewed', 'Passed', 'CONGRATS', '2026-04-12', '2026-04-12 19:21:23');
+(11, 11, 14, 'AP', 'Reviewed', 'Passed', 'CONGRATS', '2026-04-12', '2026-04-12 19:21:23'),
+(12, 14, 19, 'S A A D', 'Reviewed', 'Failed', 'SEE YOU NEXT YEAR', '2026-04-18', '2026-04-18 07:20:48'),
+(13, 14, 20, 'S A A D', 'Reviewed', 'Passed', 'CONGRATS', '2026-04-18', '2026-04-18 07:22:17'),
+(14, 15, 19, 'CODING', 'Reviewed', 'Passed', 'CONGRATS', '2026-04-18', '2026-04-18 07:27:02');
 
 -- --------------------------------------------------------
 
@@ -112,7 +115,6 @@ CREATE TABLE `teacher_album` (
 
 INSERT INTO `teacher_album` (`id`, `teacher_name`, `teacher_photo`, `teacher_email`, `teacher_contact`, `teacher_department`, `created_at`) VALUES
 (1, 'ALLETE MANZANERO', '1776320441_4380.jpg', 'alette@gmail.com', '09851642711', 'IT DEPARTMENT', '2026-04-16 06:11:44'),
-(2, 'RAYMART FALLER', '1776320840_5521.jpg', 'raymart@gmail.com', '09851642711', 'IT DEPARTMENT', '2026-04-16 06:27:20'),
 (3, 'ANGELO VILLIANUEVA', '1776320962_7238.jpg', 'angelo@gmail.com', '09851642711', 'IT DEPARTMENT', '2026-04-16 06:29:22'),
 (4, 'KENJI UY', '1776321024_9877.jpg', 'kenji@gmail.com', '09851642711', 'IT DEPARTMENT', '2026-04-16 06:30:24'),
 (5, 'DENNIS SANTOS', '1776321083_1276.jpg', 'denis@gmail.com', '09851642711', 'IT DEPARTMENT', '2026-04-16 06:31:13'),
@@ -149,7 +151,9 @@ INSERT INTO `teacher_classes` (`id`, `teacher_id`, `subject`, `course`, `class_c
 (9, 11, 'PE', 'BSIT 3', '1d9l5jy', '2026-04-12 16:05:56'),
 (10, 2, 'SCIENCE', 'BSIT 1', '2DWKS27J', '2026-04-12 17:07:13'),
 (11, 13, 'AP', 'BSIT 2', 'SVUA4CFL', '2026-04-12 19:20:08'),
-(13, 4, 'HUMAN COMPUTER INTERACTION', 'BSIT 3', 'VXZPLNYK', '2026-04-15 14:00:24');
+(13, 4, 'HUMAN COMPUTER INTERACTION', 'BSIT 3', 'VXZPLNYK', '2026-04-15 14:00:24'),
+(14, 18, 'S A A D', 'BSIT 3', 'EJCTZS83', '2026-04-18 07:18:32'),
+(15, 21, 'CODING', 'BSIT 3', 'JL5LCANK', '2026-04-18 07:26:25');
 
 -- --------------------------------------------------------
 
@@ -166,30 +170,30 @@ CREATE TABLE `users` (
   `password` varchar(255) DEFAULT NULL,
   `role` enum('student','teacher') DEFAULT NULL,
   `course` varchar(50) DEFAULT NULL,
-  `profile_photo` varchar(255) DEFAULT NULL
+  `profile_photo` varchar(255) DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
+  `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `contact_number`, `password`, `role`, `course`, `profile_photo`) VALUES
-(1, 'Mark', 'Paredes', 'markparedes54321@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 3', 'user_1_1775969467.jpg'),
-(2, 'Donalene', 'Tocmo', 'Donalyn@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, 'teacher_2_1776012678.jpg'),
-(3, 'Gian', 'Esio', 'gianesio@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 3', 'student_3_1776150209.jpg'),
-(4, 'alette', 'manzanero', 'alette@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL),
-(5, 'Kenji', 'Uy', 'kenji@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL),
-(6, 'Angelo', 'Villianueva', 'Angelo@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL),
-(7, 'Angel', 'Bernase', 'Angel@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, 'teacher_7_1776276983.png'),
-(8, 'Genalyn', 'Esio', 'genalyn@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL),
-(9, 'Dennis', 'Santos', 'dennis@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL),
-(10, 'Daryl', 'Esio', 'daryl@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL),
-(12, 'natasha', 'Esio', 'natasha@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 1', NULL),
-(13, 'CARDO', 'TANGOL', 'cardo@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, 'teacher_13_1776021746.png'),
-(14, 'JECK', 'DETERA', 'jeck@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 2', 'student_14_1776021794.png'),
-(15, 'Harlie', 'Poras', 'harlie@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 3', NULL),
-(16, 'Martin', 'Angeles', 'martin@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 3', NULL),
-(17, 'Kyle', 'Tulagan', 'kyle@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 3', NULL);
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `contact_number`, `password`, `role`, `course`, `profile_photo`, `is_deleted`, `deleted_at`) VALUES
+(1, 'Mark', 'Paredes', 'markparedes54321@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 3', 'user_1_1775969467.jpg', 0, NULL),
+(4, 'alette', 'manzanero', 'alette@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL, 0, NULL),
+(5, 'Kenji', 'Uy', 'kenji@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL, 0, NULL),
+(6, 'Angelo', 'Villianueva', 'Angelo@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL, 0, NULL),
+(7, 'Angel', 'Bernase', 'Angel@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, 'teacher_7_1776276983.png', 0, NULL),
+(9, 'Dennis', 'Santos', 'dennis@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL, 0, NULL),
+(10, 'Daryl', 'Esio', 'daryl@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL, 1, '2026-04-23 14:24:38'),
+(12, 'natasha', 'Esio', 'natasha@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 1', NULL, 0, NULL),
+(14, 'JECK', 'DETERA', 'jeck@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 2', 'student_14_1776021794.png', 0, NULL),
+(15, 'Harlie', 'Poras', 'harlie@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 3', NULL, 0, NULL),
+(16, 'Martin', 'Angeles', 'martin@gmail.com', '09602097975', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 3', NULL, 1, '2026-04-23 13:47:21'),
+(18, 'DONALENE', 'TOCMO', 'donalene@gmail.com', '09851642711', '81dc9bdb52d04dc20036dbd8313ed055', 'teacher', NULL, NULL, 1, '2026-04-23 12:58:53'),
+(19, 'GIAN', 'ESIO', 'gian@gmail.com', '09851642711', '81dc9bdb52d04dc20036dbd8313ed055', 'student', 'BSIT 3', NULL, 1, '2026-04-23 14:24:45'),
+(22, 'PATRICIO EZEKIEL', 'MAGDAME', 'patricio@gmail.com', '09602097975', '70f83b32b430dd05e2c39ff69c08c375', 'student', 'BSIT 1', 'student_22_1776927318.png', 0, NULL);
 
 --
 -- Indexes for dumped tables
@@ -252,7 +256,7 @@ ALTER TABLE `class_members`
 -- AUTO_INCREMENT for table `class_requests`
 --
 ALTER TABLE `class_requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `teacher_album`
@@ -264,13 +268,13 @@ ALTER TABLE `teacher_album`
 -- AUTO_INCREMENT for table `teacher_classes`
 --
 ALTER TABLE `teacher_classes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
